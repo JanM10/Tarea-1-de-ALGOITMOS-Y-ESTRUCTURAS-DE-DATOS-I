46c9fe8d5b65388ae38a28f95230fb52680b51ec
JanM10 <JanM10@users.noreply.github.com>
2020-09-20 12:59:04 -0600

    Se le agregaron algunas caracteristicas a la ventana del cliente como el tamano, donde aparece y que no sea redimencionable.

--- src/com/company/Cliente.java
+++ src/com/company/Cliente.java
@@ -1,13 +1,16 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Cliente {
 
     public static void main(String[] args) {
 
         miMarco marco1 = new miMarco();
+
         marco1.setVisible(true);
+
         marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
@@ -18,7 +21,21 @@ class miMarco extends JFrame{
 
     public miMarco(){
 
-        setSize(600,600);
+        Toolkit mipantalla = Toolkit.getDefaultToolkit();
+
+        Dimension tamanoPantalla = mipantalla.getScreenSize();
+
+        int alturaPantalla = tamanoPantalla.height;
+
+        int anchoPantalla = tamanoPantalla.width;
+
+        setSize(anchoPantalla/2,alturaPantalla/2);
+
+        setLocation(anchoPantalla/4,alturaPantalla/4);
+
+        setTitle("Ventana Cliente");
+
+        setResizable(false);
 
     }
 }
