8f227baa5aa16bd637c49a39923e402783d80178
JanM10 <JanM10@users.noreply.github.com>
2020-09-19 18:06:49 -0600

    Se hace visible la ventana y al correr el programa ya se logra ver.

--- src/com/company/Cliente.java
+++ src/com/company/Cliente.java
@@ -7,6 +7,8 @@ public class Cliente {
     public static void main(String[] args) {
 
         miMarco marco1 = new miMarco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
