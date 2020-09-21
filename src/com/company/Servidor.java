3dee6c3e84f501ed0bcf9d0d7003004e4bc037b3
JanM10 <JanM10@users.noreply.github.com>
2020-09-21 17:23:09 -0600

    Se crea una ventana para el servidor.

--- src/com/company/Servidor.java
+++ src/com/company/Servidor.java
@@ -1,4 +1,13 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Servidor {
+
+    public static void main(String[] args) {
+
+        marcoServidor mimarco = new marcoServidor();
+
+        mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
